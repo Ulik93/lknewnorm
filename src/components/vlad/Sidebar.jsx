62532c0e729debcd7d3c5331a499525e196eca66
Ulik93 <uliksh@gmail.com>
2021-05-12 19:59:25 +0600

    Разделы

--- src/components/vlad/Sidebar.jsx
+++ src/components/vlad/Sidebar.jsx
@@ -5,14 +5,14 @@ import './vlad.css'
 export default function Sidebar() {
     return (
         
-        <div className={x.flexbox}>
+        <div className='vlad_navbar'>
             <div className='flex-item'>
                 <NavLink className='flex-item' to="/parfume">Perfume</NavLink>
             </div>
             <div className='flex-item'>
                 <NavLink className='flex-item'  to="/shoes">Shoes</NavLink>
             </div>
-            <div className='flex-item'>
+            <div >
                 <NavLink className='flex-item' to="/clothes">Clothes</NavLink>
             </div>
         </div>
